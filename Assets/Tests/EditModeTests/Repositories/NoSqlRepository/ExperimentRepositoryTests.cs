e0f408a02bdce99a17ce8a272c4792e5884bad61
Petr Havel <siskape1@fit.cvut.cz>
2023-08-14 01:13:37 +0200

    First test for repo
    Async acceptions not fully supported under unity needed to find a workaround

--- Assets/Tests/EditModeTests/Repositories/NoSqlRepository/ExperimentRepositoryTests.cs
+++ Assets/Tests/EditModeTests/Repositories/NoSqlRepository/ExperimentRepositoryTests.cs
@@ -5,83 +5,117 @@ using UnityEngine.TestTools;
 using Microsoft.WindowsAzure.Storage.Table;
 using Moq;
 using HoloLens4Labs.Scripts.DTOs;
+using HoloLens4Labs.Scripts.Repositories;
 using System.Threading.Tasks;
 using System.Reflection;
 using System.Linq;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage;
+using System.Collections.Generic;
+using HoloLens4Labs.Scripts.Exceptions;
+
+public static class UnityTestUtils
+{
+
+    public static T RunAsyncMethodSync<T>(Func<Task<T>> asyncFunc)
+    {
+        return Task.Run(async () => await asyncFunc()).GetAwaiter().GetResult();
+    }
+    public static void RunAsyncMethodSync(Func<Task> asyncFunc)
+    {
+        Task.Run(async () => await asyncFunc()).GetAwaiter().GetResult();
+    }
+}
+
 
 public class ExperimentRepositoryTests
 {
 
-    Mock<CloudTableMock> mockTable;
-    TableQuerySegment<ExperimentDTO> mockQuerySegment;
+    
+    
+
 
     public  class CloudTableMock : CloudTable
     {
-        public CloudTableMock() : base(new Uri("http://mockuri"))
+
+
+        public CloudTableMock() : base (new Uri("http://sdfs.com")) { }
+
+       /* public CloudTableMock(Uri tabelAddress) : base(new Uri("http://mockuri"))
         {
         }
+        public CloudTableMock(StorageUri tableAddress, StorageCredentials credentials) : base(tableAddress, credentials)
+        { }
+
+        public CloudTableMock(Uri tableAbsoluteUri, StorageCredentials credentials) : base(tableAbsoluteUri, credentials)
+        { }
+
+        public async override Task<TableResult> ExecuteAsync(TableOperation operation)
+        {
+            return await Task.FromResult(new TableResult
+            {
+                Result = new ScreenSettingEntity() { Settings = "" },
+                HttpStatusCode = 200
+            });
+        }
+
+        private class ScreenSettingEntity
+        {
+            public ScreenSettingEntity()
+            {
+            }
+
+            public string Settings { get; set; }
+        }
+       */
     }
 
 
-    [SetUp]
-    public void Init()
-    {
 
-  /*      var ctor = typeof(TableQuerySegment<ExperimentDTO>)
-                .GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic)
-        .FirstOrDefault(c => c.GetParameters().Count() == 1);
-
-        mockQuerySegment = ctor.Invoke(new object[] {
-            new ExperimentDTO() {
-
-                Name = "test",
-                ExperimentID = "123"
-
-             }
-        }) as TableQuerySegment<ExperimentDTO>;
-
-        mockTable = new Mock<CloudTableMock>();
-        mockTable
-          .Setup(w => w.ExecuteQuerySegmentedAsync(It.IsAny<TableQuery<ExperimentDTO>>(),
-            It.IsAny<EntityResolver<ExperimentDTO>>(),
-            It.IsAny<TableContinuationToken>()))
-          .Returns(Task.FromResult(mockQuerySegment));
-
-        */
-
-    }
-
-
-    // A Test behaves as an ordinary method
     [Test]
-    public void ExperimentRepositoryTestsSimplePasses()
+    public void ObjectExistsException()
     {
-        // Use the Assert class to test conditions
-        var ctor = typeof(TableQuerySegment<ExperimentDTO>)
+
+
+        var experimentList = new List<ExperimentDTO>
+        {
+            new ExperimentDTO {
+                Name = "experiment1",
+                ExperimentID = "135"
+            }
+        };
+
+
+        var constructorInfo = typeof(TableQuerySegment<ExperimentDTO>)
                 .GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic)
         .FirstOrDefault(c => c.GetParameters().Count() == 1);
-        mockQuerySegment = ctor.Invoke(new object[] {
-            new System.Collections.Generic.List<ExperimentDTO> () 
+
+        var mockQuerySegment = constructorInfo.Invoke(new object[] {
+            experimentList
         }) as TableQuerySegment<ExperimentDTO>;
 
-        mockTable = new Mock<CloudTableMock>();
+        var mockTable = new Mock<CloudTableMock>();
         mockTable
           .Setup(w => w.ExecuteQuerySegmentedAsync(It.IsAny<TableQuery<ExperimentDTO>>(),
-            It.IsAny<EntityResolver<ExperimentDTO>>(),
             It.IsAny<TableContinuationToken>()))
           .Returns(Task.FromResult(mockQuerySegment));
 
+        var experimentRepository = new ExperimentRepository(mockTable.Object, "l");
+        var experimentDto = new ExperimentDTO() { 
+        
+        Name = "Experiment2",
+        ExperimentID ="246"
+        
+        };      
+
+        Assert.Throws<ObjectDataBaseException>(
+                          () => UnityTestUtils.RunAsyncMethodSync(() => experimentRepository.Create(experimentDto)));
 
 
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator ExperimentRepositoryTestsWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
+
+
 }
+
+
