814390e39aaa4479974466df8647ba9fa279ee17
Petr Havel <siskape1@fit.cvut.cz>
2024-01-23 19:41:54 +0100

    Final change So that endpoints are no longer present in the commited code
    + refreshed all endpoints and access keys

--- Assets/Tests/EditModeTests/Services/AzureImageAnalysisServiceTests.cs
+++ Assets/Tests/EditModeTests/Services/AzureImageAnalysisServiceTests.cs
@@ -7,16 +7,38 @@ using NUnit.Framework;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using HoloLens4Labs.Scripts.DTOs;
+using HoloLens4Labs.Scripts.Exceptions;
+using UnityEngine;
 
 
 public class AzureImageAnalysisServiceTests
 {
+
+    AzureServicesEndpointsDTO azureServicesEndpoints = default;
+
+    [SetUp]
+    public void Init()
+    {
+
+        var textAsset = Resources.Load<TextAsset>("AzureServicesEndpointsConfig");
+        if (textAsset == null)
+        {
+            Debug.LogError("AzureServicesEndpointsConfig.json not found in Resources folder");
+            throw new ObjectNotInitializedException("Azure services endpoint missing");
+        }
+        Debug.Log("AzureServicesEndpoints.json loaded from Resources folder" + textAsset.text);
+        azureServicesEndpoints = JsonUtility.FromJson<AzureServicesEndpointsDTO>(textAsset.text);
+
+
+    }
+
     // A Test behaves as an ordinary method
     [Test]
     public void ImageWithVialSuccess()
     {
-        string key = "00aea6c033354ee7b9a3c54e423846a7";
-        string endpoint = "https://westeurope.api.cognitive.microsoft.com";
+        string key = azureServicesEndpoints.ImageAnalysisKey;
+        string endpoint = azureServicesEndpoints.ImageAnalysisEndpoint;
 
         ImageData imageData = new ImageData("1", DateTime.Now, "1", "1",
             _ => Task.FromResult(File.ReadAllBytes("Assets\\Tests\\EditModeTests\\TestImages\\GlacialAcetic-GHS.jpg")));
@@ -34,8 +56,8 @@ public class AzureImageAnalysisServiceTests
     [Test]
     public void ImageWithMultipleLineTextSuccess()
     {
-        string key = "00aea6c033354ee7b9a3c54e423846a7";
-        string endpoint = "https://westeurope.api.cognitive.microsoft.com";
+        string key = azureServicesEndpoints.ImageAnalysisKey;
+        string endpoint = azureServicesEndpoints.ImageAnalysisEndpoint;
 
         ImageData imageData = new ImageData("1", DateTime.Now, "1", "1",
             _=> Task.FromResult(File.ReadAllBytes("Assets\\Tests\\EditModeTests\\TestImages\\radomSticky.jpg")));
